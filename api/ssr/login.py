1dc9f6258fced77445f811736fc94f91bb6cfa4f
starvii <starvii@gmail.com>
2022-06-20 00:47:04 +0800

    use enc cookie instead a lot of session

--- api/ssr/login.py
+++ api/ssr/login.py
@@ -13,13 +13,15 @@ import struct
 from typing import Optional, Tuple
 from urllib.parse import urlparse
 
-from sanic import Sanic, Blueprint, Request, response, HTTPResponse
+from sanic import Blueprint, Request, response, HTTPResponse
 from sqlalchemy import select
 
 from .__ssr__ import render
 from .. import Context, get_context
 from ..utility.captcha import get_captcha
 from ..utility.constant import Constant
+from ..utility.external import pynacl_util
+from ..utility.external.base_x import base255
 from ..utility.external.google_token import get_totp_token
 from ..utility.external.pynacl_util import password_verify
 from ..v1.model.platform import Account
@@ -32,21 +34,29 @@ bp: Blueprint = Blueprint('login_web', 'account', strict_slashes=False)
 @bp.route('/login.html', strict_slashes=False)
 async def login_with_code_page(_: Request):
     """ 使用账号、密码登录 """
-    return response.html(render('account/login0code.html'))
+    res = response.html(render('account/login0code.html'))
+    del res.cookies['s']
+    return res
 
 
 @bp.route('/login-with-portal.html', strict_slashes=False)
 async def login_with_portal_page(_: Request):
-    return response.html(render('account/login0portal.html'))
+    res = response.html(render('account/login0portal.html'))
+    del res.cookies['s']
+    return res
 
 
 @bp.route('/register.html', strict_slashes=False)
 async def register_page(_: Request):
-    return response.html(render('account/register.html'))
+    res = response.html(render('account/register.html'))
+    del res.cookies['s']
+    return res
 
 
 class LoginVO:
     def __init__(self) -> None:
+        self.session_token: str = base64.urlsafe_b64encode(secrets.token_bytes(9)).decode()
+
         self.form_user: str = ''
         self.form_pass: str = ''
 
@@ -75,6 +85,61 @@ class LoginVO:
         )
         self.db_token = base64.b32encode(os.urandom(10)).decode().lower()
         self.has_token = True
+
+    def serialize(self) -> bytes:
+        """ serialize and zip
+        1  bytes        ctrl
+        9  bytes        session_token
+        16 bytes        password_salt
+        32 bytes        password_mac
+        10 bytes        totp_token
+        5  bytes        session_captcha(or 0 bytes)
+        1  bytes        \x00
+            base255(form_user)
+        1  bytes        \x00
+            base255(form_pass)
+        """
+        buf = bytearray()
+        buf.append(self._ctrl_byte & 0xff)
+        buf.extend(base64.urlsafe_b64decode(self.session_token))
+        buf.extend(struct.pack('>i', self.db_id))
+        salt, mac = self.db_password_hash.split('$')
+        buf.extend(base64.b64decode(salt + '=='))
+        buf.extend(base64.b64decode(mac + '='))
+        token = os.urandom(10) \
+            if self.db_token is None or len(self.db_token) != 16 else \
+            base64.b32decode(self.db_token.upper())
+        buf.extend(token)
+        buf.extend(self.session_captcha.encode())
+        buf.append(0)
+        buf.extend(base255.encode(self.form_user.encode()))
+        buf.append(0)
+        buf.extend(base255.encode(self.form_pass.encode()))
+        return bytes(buf)
+
+    def deserialize(self, stream: bytes) -> None:
+        s = stream
+        self._ctrl_byte = s[0]
+        s = s[1:]
+        self.session_token = base64.urlsafe_b64encode(s[:9]).decode()
+        s = s[9:]
+        self.db_id = struct.unpack('>i', s[:4])[0]
+        s = s[4:]
+        salt = s[:16]
+        s = s[16:]
+        mac = s[:32]
+        s = s[32:]
+        self.db_password_hash = '{}${}'.format(
+            base64.b64encode(salt).decode().replace('=', ''),
+            base64.b64encode(mac).decode().replace('=', ''),
+        )
+        token = s[:10]
+        s = s[10:]
+        self.db_token = base64.b32encode(token).decode().lower()
+        a = s.split(b'\x00')
+        self.session_captcha = a[0].decode()
+        self.form_user = base255.decode(a[1]).decode()
+        self.form_pass = base255.decode(a[2]).decode()
 
     @property
     def fake(self) -> bool:
@@ -128,6 +193,7 @@ class LoginUtil:
             a.init_with_account(db_id, ph, token)
         else:
             a.init_fake()
+        a.form_user = code
         return a
 
     @staticmethod
@@ -136,7 +202,6 @@ class LoginUtil:
         username = request.form.get('code')
         password = request.form.get('pass')
         lvo = await LoginUtil.step_0_query_account(request, username)
-        lvo.form_user = username
         lvo.form_pass = password
         if lvo.fake:  # 未查到用户，根据用户名随机使用校验方式。
             if not LoginUtil.is_username_legal(username):
@@ -156,26 +221,40 @@ class LoginUtil:
             res = response.html(render('account/login1captcha.html', url=url))
         else:
             res = response.html(render('account/login1token.html'))
-        session = request.ctx.web_session
-        if Constant.SESSION_NAME_LOGIN in session:
-            del session[Constant.SESSION_NAME_LOGIN]
-        session[Constant.SESSION_NAME_LOGIN] = lvo.__dict__
+
+        web_session = request.ctx.web_session
+        if Constant.SESSION_NAME_LOGIN in web_session:
+            del web_session[Constant.SESSION_NAME_LOGIN]
+        web_session[Constant.SESSION_NAME_LOGIN] = lvo.session_token
+        enc_cookie = pynacl_util.encrypt(ctx.secret, lvo.serialize())
+        res.cookies['s'] = base64.urlsafe_b64encode(enc_cookie).decode()
+        res.cookies['s']['httponly'] = True
         return res
 
     @staticmethod
     async def step_2_verify_factors(request: Request) -> Tuple[int, HTTPResponse]:
-        session = request.ctx.web_session
         lvo = LoginVO()
-        lvo.__dict__ = session[Constant.SESSION_NAME_LOGIN]
-        del session[Constant.SESSION_NAME_LOGIN]
+        web_session = request.ctx.web_session
+        try:
+            enc_cookie = request.cookies['s']
+            stream = pynacl_util.decrypt(ctx.secret, base64.urlsafe_b64decode(enc_cookie))
+            lvo.deserialize(stream)
+            if not secrets.compare_digest(web_session[Constant.SESSION_NAME_LOGIN], lvo.session_token):
+                raise ValueError('tokens are different between cookie and session')
+        except Exception as e:
+            _ = e  # 没有加密信息或者解密失败。
+            lvo.init_fake()
+        if Constant.SESSION_NAME_LOGIN in web_session:
+            del web_session[Constant.SESSION_NAME_LOGIN]
 
         capt: str = request.form.get('capt')
         totp: str = request.form.get('auth')
+        result = None
         if capt:
             ret = secrets.compare_digest(capt.lower(), lvo.session_captcha)
             if not ret:
                 # 图形验证码错误
-                return -1, response.html(render(
+                result = -1, response.html(render(
                     'message/uni-message.html',
                     title='captcha',
                     panel_title='错误',
@@ -186,7 +265,7 @@ class LoginUtil:
             ret = secrets.compare_digest(totp, get_totp_token(lvo.db_token))
             if not ret:
                 # 谷歌时间令牌错误
-                return -2, response.html(render(
+                result = -2, response.html(render(
                     'message/uni-message.html',
                     title='google time-based one-time password',
                     panel_title='错误',
@@ -194,33 +273,38 @@ class LoginUtil:
                     back_url='/',
                 ))
         else:
-            return -3, response.html(render(
+            result = -3, response.html(render(
                 'message/uni-message.html',
                 title='unknown',
                 panel_title='错误',
                 panel_message='未知错误！请重新登录！',
                 back_url='/',
             ))
-        # 校验密码
-        ret = password_verify(lvo.db_password_hash, lvo.form_pass)
-        f0 = 1 if ret else 0
-        f1 = 0 if lvo.fake else 1
-        ff = f0 & f1
-        if ff == 0:
-            return -4, response.html(render(
-                'message/uni-message.html',
-                title='credential',
-                panel_title='错误',
-                panel_message='用户名或密码错误！请重新登录！',
-                back_url='/',
-            ))
-        return lvo.db_id, response.html(render(
-            'message/uni-message.html',
-            title='welcome',
-            panel_title=f'欢迎你，{lvo.form_user}！',
-            panel_message='登录成功，请继续。',
-            back_url='/',
-        ))
+        # 强化校验没有发生错误，开始进行密码校验
+        if result is None:
+            ret = password_verify(lvo.db_password_hash, lvo.form_pass)
+            f0 = 1 if ret else 0
+            f1 = 0 if lvo.fake else 1
+            ff = f0 & f1
+            if ff == 0:
+                result = -4, response.html(render(
+                    'message/uni-message.html',
+                    title='credential',
+                    panel_title='错误',
+                    panel_message='用户名或密码错误！请重新登录！',
+                    back_url='/',
+                ))
+            else:
+                result = lvo.db_id, response.html(render(
+                    'message/uni-message.html',
+                    title='welcome',
+                    panel_title=f'欢迎你，{lvo.form_user}！',
+                    panel_message='登录成功，请继续。',
+                    back_url='/',
+                ))
+        res: HTTPResponse = result[1]
+        del res.cookies['s']
+        return result[0], res
 
     @staticmethod
     def is_username_legal(login_name: str) -> bool:
@@ -252,10 +336,8 @@ async def login(request: Request):
         # 来自第二步校验。校验验证码，通过后继续校验密码。
         uid, res = await LoginUtil.step_2_verify_factors(request)
         if uid > 0:
-            # account: Account = await LoginUtil.step_3_query_account(request, uid)
-            # print(account.__dict__)
-            session = request.ctx.web_session
-            session[Constant.SESSION_NAME_CURRENT_ACCOUNT] = uid
+            web_session = request.ctx.web_session
+            web_session[Constant.SESSION_NAME_CURRENT_ACCOUNT] = uid
         return res
 
 
---
+++ api/utility/external/base_x.py
@@ -0,0 +1,65 @@
+from typing import Tuple, Optional, List
+
+
+class BaseX:
+    def __init__(self, trans_table: bytes):
+        self._enc_table: bytes = trans_table
+        self._dec_table: List[Optional[int]] = self.make_dec_table(self._enc_table)
+
+    @staticmethod
+    def make_dec_table(table: bytes) -> List[Optional[int]]:
+        buf: List[Optional[int]] = [None for _ in range(256)]
+        for i, x in enumerate(table):
+            buf[x] = i
+        return buf
+
+    def encode(self, plain: bytes) -> bytes:
+        n = int.from_bytes(plain, 'big', signed=False)
+        buf = bytearray()
+        while 1:
+            if n < len(self._enc_table):
+                buf.append(self._enc_table[n])
+                break
+            m = n % len(self._enc_table)
+            n = n // len(self._enc_table)
+            buf.append(self._enc_table[m])
+        return bytes(buf[::-1])
+
+    def decode(self, enc: bytes) -> bytes:
+        buf = []
+        s = enc
+        for i, b in enumerate(s):
+            if self._dec_table[b] is None:
+                raise ValueError(f'Non-base{len(self._enc_table)} digit found at {i}: {enc[i]}')
+            buf.append(self._dec_table[b])
+        z = 0
+        for b in buf:
+            z = z * len(self._enc_table) + b
+        n, rem = divmod(z.bit_length(), 8)
+        if rem:
+            n += 1
+        return z.to_bytes(n, 'big', signed=False)
+
+
+base255 = BaseX(bytes([x for x in range(1, 256)]))
+
+
+def __test__():
+    import string
+    base100 = BaseX(string.printable.encode())
+    a = base100.encode(b'HelloWorld!')
+    print(len(a), repr(a))
+    b = base100.decode(a)
+    print(b)
+
+    a = base255.encode(b'HelloWorld!')
+    print(len(a), repr(a))
+    b = base255.decode(a)
+    print(b)
+    # c = decode('1inndcn1p5hsvnsea7emrduj8t')
+    # print(len(c), c)
+
+
+if __name__ == '__main__':
+    __test__()
+
