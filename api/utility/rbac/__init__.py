d643b65c33bfd596f46ae60145d0ffce0e98018c
starvii <starvii@gmail.com>
2022-06-21 08:39:38 +0800

    rbac

--- api/utility/rbac/__init__.py
+++ api/utility/rbac/__init__.py
@@ -1,1 +1,8 @@
 # 参考 https://www.jianshu.com/p/86f012e72c24
+
+import casbin
+from pathlib import Path
+
+conf = Path(__file__).parent.joinpath('model.conf')
+
+e = casbin.Enforcer(str(conf), ...)  # TODO
